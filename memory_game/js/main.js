d80ea44f5cc1abf9e371af2ce610fd092d003796
Alantre Mack <alantremack@Alantres-MacBook-Pro.local>
2019-08-24 23:11:56 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,13 @@
+console.log("Up and Running!");
+
+var cardOne = "king";
+
+var cardTwo = "king";
+
+var cardThree = "queen";
+
+var cardFour = "queen";
+
+console.log("User flipped" + " " + cardOne);
+
+console.log("User flipped" + " " + cardTwo);
